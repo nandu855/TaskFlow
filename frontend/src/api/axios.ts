5a15f9d75fe41da84273a1e94c04bdf2fe269cad
nandu855 <127336035+nandu855@users.noreply.github.com>
2026-07-07 15:33:35 +0530

    Fix API URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskflow-7tf4.onrender.com",
+  baseURL: "https://taskflow-7tf4.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
